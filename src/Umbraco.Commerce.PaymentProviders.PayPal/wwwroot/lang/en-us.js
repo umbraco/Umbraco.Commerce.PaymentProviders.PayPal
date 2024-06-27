c81867e02c305c62accdd8efcdd1a8c0c0e8f28d
Dinh Tran <trd@umbraco.dk>
2024-06-27 16:16:22 +0700

    upgrade to commerce v14

---
+++ src/Umbraco.Commerce.PaymentProviders.PayPal/wwwroot/lang/en-us.js
@@ -0,0 +1,42 @@
+export default {
+    paymentProviders: {
+        'paypalCheckoutOnetimeLabel': 'PayPal Checkout (One Time)',
+        'paypalCheckoutOnetimeDescription': 'PayPal Checkout payment provider for one time payments',
+        'paypalCheckoutOnetimeSettingsContinueUrlLabel': 'Continue URL',
+        'paypalCheckoutOnetimeSettingsContinueUrlDescription': 'The URL to continue to after this provider has done processing. eg: /continue/',
+        'paypalCheckoutOnetimeSettingsCancelUrlLabel': 'Cancel URL',
+        'paypalCheckoutOnetimeSettingsCancelUrlDescription': 'The URL to return to if the payment attempt is canceled. eg: /cancel/',
+        'paypalCheckoutOnetimeSettingsErrorUrlLabel': 'Error URL',
+        'paypalCheckoutOnetimeSettingsErrorUrlDescription': 'The URL to return to if the payment attempt errors. eg: /error/',
+
+        'paypalCheckoutOnetimeSettingsSandboxClientIdLabel': 'Sandbox Client ID',
+        'paypalCheckoutOnetimeSettingsSandboxClientIdDescription': 'Your sandbox PayPal client id',
+
+        'paypalCheckoutOnetimeSettingsSandboxSecretLabel': 'Sandbox Secret',
+        'paypalCheckoutOnetimeSettingsSandboxSecretDescription': 'Your sandbox PayPal secret',
+
+        'paypalCheckoutOnetimeSettingsSandboxWebhookIdLabel': 'Sandbox Webhook ID',
+        'paypalCheckoutOnetimeSettingsSandboxWebhookIdDescription': 'Your sandbox PayPal webhook id',
+
+        'paypalCheckoutOnetimeSettingsLiveClientIdLabel': 'Live Client ID',
+        'paypalCheckoutOnetimeSettingsLiveClientIdDescription': 'Your live PayPal client id',
+
+        'paypalCheckoutOnetimeSettingsLiveSecretLabel': 'Live Secret',
+        'paypalCheckoutOnetimeSettingsLiveSecretDescription': 'Your live PayPal secret',
+
+        'paypalCheckoutOnetimeSettingsLiveWebhookIdLabel': 'Live Webhook ID',
+        'paypalCheckoutOnetimeSettingsLiveWebhookIdDescription': 'Your live PayPal webhook id',
+
+        'paypalCheckoutOnetimeSettingsSandboxModeLabel': 'Sandbox Mode',
+        'paypalCheckoutOnetimeSettingsSandboxModeDescription': 'Set whether to process payments in sandbox mode',
+
+        'paypalCheckoutOnetimeSettingsBrandNameLabel': 'Brand Name',
+        'paypalCheckoutOnetimeSettingsBrandNameDescription': 'A brand name to override the business name with on the PayPal Checkout pages',
+
+        'paypalCheckoutOnetimeSettingsOrderDescriptionLabel': 'Order Description',
+        'paypalCheckoutOnetimeSettingsOrderDescriptionDescription': 'A description to display next to the PayPal order line, defaults to the order number if not set',
+
+        'paypalCheckoutOnetimeSettingsCaptureLabel': 'Capture',
+        'paypalCheckoutOnetimeSettingsCaptureDescription': 'Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture',
+    },
+};
